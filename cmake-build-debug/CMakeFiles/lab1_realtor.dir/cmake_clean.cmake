e0f46c6747ac199913ebe7a6764d1252cfc35ebe
Bohdan Tsymbal <tsymbal.lit@gmail.com>
2021-11-03 11:50:13 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/lab1_realtor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_realtor.dir/main.cpp.obj"
+  "lab1_realtor.exe"
+  "lab1_realtor.exe.manifest"
+  "lab1_realtor.pdb"
+  "liblab1_realtor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_realtor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
